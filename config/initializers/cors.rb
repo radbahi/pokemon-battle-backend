b4f820626b11b499c3d309116c3b341a4afe5d9f
Radouane Bahi <radbahi@gmail.com>
2020-12-06 12:02:30 -0500

    cors origin update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'pokemon-battle-frontend.herokuapp.com'
+    origins 'https://radbahi.github.io'
 
     resource '*',
       headers: :any,
